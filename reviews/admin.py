f07f3442a92a9afd2bf3215c419fae277919f3c0
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-01-17 18:24:38 +0000

    Install Summernote and apply constraints for the admin panel

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Review, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Review)
+class ReviewAdmin(SummernoteModelAdmin):
+    list_display = ('author', 'rating', 'status', 'created_on')
+    search_fields = ['content']
+    list_filter = ('status', 'created_on',)
+    summernote_fields = ('content',)
+
 
 # Register your models here.
-admin.site.register(Review)
 admin.site.register(Comment)
