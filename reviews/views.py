3c8ed0274661ba34c6402883b0baf2791814fc55
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-01-18 15:36:47 +0000

    Create base template and reviews page and insert temporary CSS styling

--- reviews/views.py
+++ reviews/views.py
@@ -5,4 +5,5 @@ from .models import Review
 # Create your views here.
 class ReviewList(generic.ListView):
     queryset = Review.objects.filter(status=1).order_by("-created_on")
-    template_name = "review_list.html"
+    template_name = "reviews/reviews.html"
+    paginate_by = 10
