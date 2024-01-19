1d65a730bfaa89f1be076ecbd92b211704190798
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-01-19 17:24:14 +0000

    Add review_detail view

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ReviewList.as_view(), name='reviews'),
+    path('reviews/<int:review_id>/', views.review_detail, name='review_detail'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Review
 
@@ -7,3 +7,27 @@ class ReviewList(generic.ListView):
     queryset = Review.objects.filter(status=1).order_by("-created_on")
     template_name = "reviews/reviews.html"
     paginate_by = 10
+
+
+def review_detail(request, review_id):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`reviews.Review`.
+
+    **Template:**
+
+    :template:`reviews/review_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    review = get_object_or_404(Review, id=review_id)
+
+    return render(
+        request,
+        "reviews/review_detail.html",
+        {'review': review},
+    )
