34d6ad5bd80f9907e0311841d6866bacc9db9ccd
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-01-23 14:13:27 +0000

    Add logic for users to post comments on reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Review
+from .forms import CommentForm
 
 # Create your views here.
 class ReviewList(generic.ListView):
@@ -28,6 +30,20 @@ def review_detail(request, review_id):
     comments = review.comments.all().order_by("-replied_on")
     comment_count = review.comments.filter(approved=True).count()
 
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.review = review
+            comment.save()
+            messages.add_message(
+            request, messages.SUCCESS,
+            'Your comment was submitted and is now pending admin approval'
+        )
+
+    comment_form = CommentForm()
+
     return render(
         request,
         "reviews/review_detail.html",
@@ -35,5 +51,6 @@ def review_detail(request, review_id):
             'review': review,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         },
     )
