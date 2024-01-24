17e8cae84bf2f060e0bd8eaaab43624efb35ce27
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-01-24 15:16:57 +0000

    Setup Cloudinary

--- reviews/models.py
+++ reviews/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 RATING_CHOICES = ((1, "1 star"), (2, "2 stars"), (3, "3 stars"), (4, "4 stars"), (5, "5 stars"))
 
