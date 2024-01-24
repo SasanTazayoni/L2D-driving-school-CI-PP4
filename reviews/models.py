6919a3d14899786211084193e4a08539714a4dae
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-01-24 14:53:02 +0000

    Change Review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-STATUS = ((0, "Draft"), (1, "Published"))
 RATING_CHOICES = ((1, "1 star"), (2, "2 stars"), (3, "3 stars"), (4, "4 stars"), (5, "5 stars"))
 
 
@@ -10,8 +9,8 @@ class Review(models.Model):
     author = models.OneToOneField(User, on_delete=models.CASCADE, related_name="review", null=False, blank=False)
     rating = models.IntegerField(choices=RATING_CHOICES, null=False, blank=False)
     content = models.TextField(null=False, blank=False)
+    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
--- reviews/views.py
+++ reviews/views.py
@@ -6,7 +6,7 @@ from .forms import CommentForm
 
 # Create your views here.
 class ReviewList(generic.ListView):
-    queryset = Review.objects.filter(status=1).order_by("-created_on")
+    queryset = Review.objects.filter(approved=True).order_by("-created_on")
     template_name = "reviews/reviews.html"
     paginate_by = 10
 
@@ -25,7 +25,7 @@ def review_detail(request, review_id):
     :template:`reviews/review_detail.html`
     """
 
-    queryset = Review.objects.filter(status=1)
+    queryset = Review.objects.filter(approved=True)
     review = get_object_or_404(Review, id=review_id)
     comments = review.comments.all().order_by("-replied_on")
     comment_count = review.comments.filter(approved=True).count()
