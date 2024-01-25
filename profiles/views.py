6a08f9603dc04b3e9aaf4716a7f1fcb3991d757a
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-01-25 11:31:18 +0000

    Create profiles view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import UserProfile
 
-# Create your views here.
+
+def profile_page(request):
+    """
+    Renders the profile page
+    """
+
+    profile = UserProfile.objects.get(user=request.user)
+
+    return render(
+        request,
+        'user_profile.html',
+        {'profile': profile}
+    )
