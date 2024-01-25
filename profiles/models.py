16cd9cceae70b7f4e6652d81ea137b3f51a06b54
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-01-25 12:57:58 +0000

    Remove default message for user picture

--- profiles/models.py
+++ profiles/models.py
@@ -7,7 +7,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=False, blank=False)
     username = models.CharField(max_length=255, blank=False, null=False)
     email = models.EmailField(blank=False, null=False)
-    profile_picture = CloudinaryField('image', default='defaultuser', null=True)
+    profile_picture = CloudinaryField('image', null=True)
     about_me = models.TextField(blank=False, null=True)
     has_reviewed = models.BooleanField(default=False)
 
