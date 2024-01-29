d034e20bf45ec51005fec10275d8a73e772f69a3
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-01-29 18:25:04 +0000

    Move custom JS for displaying uploaded images into separate file

---
+++ static/js/profile.js
@@ -0,0 +1,13 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (window.location.pathname.endsWith('/profile/edit/')) {
+        // select the image div containers, and get the image href/url
+        const imageDiv = document.querySelector('#div_id_profile_picture')
+        const imageSrcDiv = imageDiv.querySelector('.input-group')
+        const imageSrc = imageDiv.querySelector('a')
+
+        // change the image src div to show the image
+        imageSrcDiv.innerHTML = `<div class="col-12">Current Image:<img style="display: block;" src="${imageSrc.href}" /></div>`
+    }
+})
