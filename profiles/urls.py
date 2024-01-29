9dcb4a9dadd91b3d673718d3dccf0021f0c3f761
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-01-29 14:08:09 +0000

    Create the url path for the edit profile page

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,8 @@
 from . import views
-from .views import profile_page
+from .views import profile_page, edit_profile
 from django.urls import path
 
 urlpatterns = [
     path('', profile_page, name='profile_page'),
+    path('edit/', edit_profile, name='edit_profile'),
 ]
