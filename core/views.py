f49d18f06e558b32ec5fd019bbf6ce498bbec5e7
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-02-01 17:13:23 +0000

    Add logic so that only authenticated users can see appointments tab

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -8,9 +9,11 @@ def home(request):
     return render(request, 'core/index.html')
 
 
+@login_required(login_url='login')
 def appointments(request):
     """
-    Renders the appointments page
+    Renders the appointments page if the user is authenticated,
+    otherwise redirects to the login page.
     """
     return render(request, 'core/appointments.html')
 
