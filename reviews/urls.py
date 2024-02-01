774a22cff6632e35f44b5cbeed991ec3e018fcfe
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-02-01 13:40:32 +0000

    Establish clearer file path for each individual review

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ReviewList.as_view(), name='reviews'),
-    path('reviews/<int:review_id>/', views.review_detail, name='review_detail'),
+    path('<int:review_id>/', views.review_detail, name='review_detail'),
 ]
