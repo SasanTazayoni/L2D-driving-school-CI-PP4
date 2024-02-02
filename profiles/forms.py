4b312de62461b5d6746c51af1a49901e7c86b942
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-02-02 13:43:26 +0000

    Adjust fields for profile editing

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,8 +1,13 @@
-from .models import UserProfile
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import UserProfile
+from django.forms import ModelForm
 
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('username', 'email', 'profile_picture', 'about_me')
+        fields = ('email', 'profile_picture', 'about_me')
+
+
