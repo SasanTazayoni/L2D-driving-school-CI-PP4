5630d04383530b704874276c8ac1d9ffd44c1fb5
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-02-02 15:13:22 +0000

    Reorder imports in models.py for profiles app

--- profiles/models.py
+++ profiles/models.py
@@ -1,8 +1,8 @@
-from django.db import models
-from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.db import models
 
 
 class UserProfile(models.Model):
