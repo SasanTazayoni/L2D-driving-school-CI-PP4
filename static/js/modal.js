2d9c02b13da0ed39462a8c1106cc6d3c8404297e
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-02-09 11:07:12 +0000

    Move logout/login/sign up button back into navbar, adjust styles and functionality

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,8 +1,14 @@
-const logoutLink = document.getElementById('logoutLink')
+const logoutLink = document.querySelector('#logoutLink')
+const logoutBtn = document.querySelector('#logoutBtn')
 const logoutModal = new bootstrap.Modal(document.getElementById('logoutConfirmationModal'))
 const closeModalButton = document.getElementById('closeModalButton')
 
 logoutLink.addEventListener('click', (e) => {
+    e.preventDefault()
+    logoutModal.show()
+})
+
+logoutBtn.addEventListener('click', (e) => {
     e.preventDefault()
     logoutModal.show()
 })
