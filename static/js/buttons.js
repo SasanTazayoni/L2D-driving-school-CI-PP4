2f904d03dacb43a0e3e5b034e79e0a30db9297df
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-02-09 17:43:10 +0000

    Add button animation

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -1,4 +1,5 @@
 const authBtns = document.querySelectorAll('.auth-button')
+const blueBtns = document.querySelectorAll('.blue-button')
 
 authBtns.forEach(btn => {
     btn.onmousemove = function(e) {
@@ -8,4 +9,21 @@ authBtns.forEach(btn => {
         btn.style.setProperty('--x', x + 'px')
         btn.style.setProperty('--y', y + 'px')
     }
+})
+
+console.log(blueBtns)
+
+blueBtns.forEach(btn => {
+    btn.addEventListener('mouseover', function (e) {
+        let x = e.clientX - e.target.getBoundingClientRect().left
+        let y = e.clientY - e.target.getBoundingClientRect().top
+        let ripples = document.createElement('span')
+        ripples.style.left = x + 'px'
+        ripples.style.top = y + 'px'
+        this.appendChild(ripples)
+
+        setTimeout(() => {
+            ripples.remove()
+        }, 600)
+    })
 })
