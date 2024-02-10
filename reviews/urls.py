102cddabdf9c005aa8ad1df8ceb69bb016af34bb
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-02-10 17:55:31 +0000

    Url bugfix

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.ReviewList.as_view(), name="reviews"),
-    path("<str:review_id>/", views.review_detail, name="review_detail"),
+    path("<int:review_id>/", views.review_detail, name="review_detail"),
     path("create-review/", views.create_review, name="create-review"),
 ]
