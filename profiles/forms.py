15858c5b886b6f95c41d332f71af14695a65ae28
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-02-12 14:57:10 +0000

    Make the first_name and email fields mandatory

--- profiles/forms.py
+++ profiles/forms.py
@@ -19,6 +19,11 @@ class ReviewForm(ModelForm):
 
 
 class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['first_name'].required = True
+        self.fields['email'].required = True
+
     class Meta:
         model = User
         fields = ['first_name', 'email', 'username', 'password1', 'password2']
