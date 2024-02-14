4253ae151c95f7e50f8d8844a25cc879fb5acdda
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-02-14 09:40:37 +0000

    Add url path for delete review and ensure consistent naming convention

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path("", views.ReviewList.as_view(), name="reviews"),
     path("<int:review_id>/", views.review_detail, name="review_detail"),
-    path("create-review/", views.create_review, name="create-review"),
-    path('update-review/<int:review_id>/', views.update_review, name='update-review'),
+    path("create-review/", views.create_review, name="create_review"),
+    path('update-review/<int:review_id>/', views.update_review, name='update_review'),
+    path('delete-review/<int:review_id>/', views.delete_review, name='delete_review'),
 ]
