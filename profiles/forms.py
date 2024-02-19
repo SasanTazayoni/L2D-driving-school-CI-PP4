41c3c28a8f481c147a3517c1668e65e425933ec8
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-02-19 17:54:29 +0000

    Adjust UserProfileForm and remove unnecessary form

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,40 +1,18 @@
 from django.core.exceptions import ValidationError
 from django import forms
 from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import UserProfile
 from reviews.models import Review
-
-
-class CustomUserCreationForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['first_name'].required = True
-        self.fields['email'].required = True
-        self.fields['first_name'].widget.attrs['autofocus'] = True
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        if User.objects.filter(email=email).exists():
-            raise ValidationError("This email address is already in use.")
-        return email
-
-    class Meta:
-        model = User
-        fields = ['first_name', 'email', 'username', 'password1', 'password2']
-        labels = {
-            'first_name': 'Name',
-            'email': 'Email'
-        }
+from .models import UserProfile
 
 
 class UserProfileForm(forms.ModelForm):
+    name = forms.CharField(max_length=200)
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'readonly': 'readonly'})
+    )
+    email = forms.CharField(max_length=256)
+
     class Meta:
         model = UserProfile
-        fields = ['first_name', 'age', 'occupation', 'email', 'profile_picture', 'about_me']
-        labels = {
-            'first_name': 'Name',
-            'email': 'Email'
-        }
-        
+        fields = ['username', 'name', 'age', 'occupation', 'email', 'about_me', 'profile_picture']
