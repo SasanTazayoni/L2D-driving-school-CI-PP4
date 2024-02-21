f1a67e4838fcb00dbaac9713d9e0a0a5628af7e0
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-02-21 21:27:39 +0000

    Adjust core urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.user_profiles, name="user_profiles"),
-    path("profile/<str:user_id>/", views.profile_detail, name="profile_detail"),
+    path("<int:user_id>/", views.profile_detail, name="profile_detail"),
 ]
