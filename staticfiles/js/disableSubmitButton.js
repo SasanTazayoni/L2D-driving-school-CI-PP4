6ac2645d6daa0618b858c2cf6197ae203826c536
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-02-29 19:49:32 +0000

    Collect static files and deploy to Heroku

---
+++ staticfiles/js/disableSubmitButton.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var submitButton = document.getElementById("submitButton")
+    var resetForm = document.getElementById("resetForm")
+
+    submitButton.addEventListener("click", function() {
+        submitButton.disabled = true
+        resetForm.submit()
+    })
+
+    setTimeout(function() {
+        submitButton.disabled = false
+    }, 3000)
+})
