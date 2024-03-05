5d69eba180b584cd12a23f92e0336ffbc079dbba
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-03-05 13:01:00 +0000

    Improve admin panel controls

--- reviews/admin.py
+++ reviews/admin.py
@@ -5,11 +5,15 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Review)
 class ReviewAdmin(SummernoteModelAdmin):
-    list_display = ('author', 'rating', 'approved', 'created_on')
-    search_fields = ['content']
-    list_filter = ('approved', 'created_on',)
-    summernote_fields = ('content',)
+    list_display = ('author', 'rating', 'approved', 'created_on', 'updated_on')
+    search_fields = ['author', 'content']
+    list_filter = ('approved', 'created_on', 'updated_on')
+    summernote_fields = ('content')
 
 
-# Register your models here.
-admin.site.register(Comment)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'review', 'approved', 'replied_on', 'updated_on')
+    search_fields = ['author', 'content']
+    list_filter = ('approved', 'replied_on', 'updated_on')
+    summernote_fields = ('content')
