de168db6e913751c009095a386227b02b000983d
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-03-05 14:13:57 +0000

    Approve admin panel for user profiles

--- profiles/admin.py
+++ profiles/admin.py
@@ -5,4 +5,15 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(UserProfile)
 class UserProfileAdmin(SummernoteModelAdmin):
+    list_display = ('get_name', 'created_on', 'approved')
+    search_fields = ['user__first_name', 'about_me']
     summernote_fields = ('about_me',)
+    actions = ['approve_profiles']
+
+    def get_name(self, obj):
+        return obj.user.first_name
+    get_name.short_description = 'Name'
+
+    def approve_profiles(self, request, queryset):
+        updated_count = queryset.update(approved=True)
+        self.message_user(request, f'{updated_count} profiles have been approved.')
