3788830aa27e15949b2413b2d0847bdc151263c2
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-03-11 14:18:05 +0000

    Add semicolons to JS files

--- static/js/disableSubmitButton.js
+++ static/js/disableSubmitButton.js
@@ -1,15 +1,15 @@
 /* jshint esversion: 11 */
 
 document.addEventListener("DOMContentLoaded", function() {
-    var submitButton = document.getElementById("submitButton")
-    var resetForm = document.getElementById("resetForm")
+    var submitButton = document.getElementById("submitButton");
+    var resetForm = document.getElementById("resetForm");
 
     submitButton.addEventListener("click", function() {
-        submitButton.disabled = true
-        resetForm.submit()
-    })
+        submitButton.disabled = true;
+        resetForm.submit();
+    });
 
     setTimeout(function() {
-        submitButton.disabled = false
-    }, 3000)
-})
+        submitButton.disabled = false;
+    }, 3000);
+});
--- static/js/editProfile.js
+++ static/js/editProfile.js
@@ -2,32 +2,32 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     if (window.location.pathname.endsWith('/profile/edit/')) {  
-        const imageDiv = document.querySelector('#profile_picture-clear_id')
-        const imageDivParent = imageDiv.parentNode
-        const firstAnchorChild = imageDivParent.querySelector('a')
-        const textNode = document.createTextNode('Current: ')
-        let hrefValue = ''
+        const imageDiv = document.querySelector('#profile_picture-clear_id');
+        const imageDivParent = imageDiv.parentNode;
+        const firstAnchorChild = imageDivParent.querySelector('a');
+        const textNode = document.createTextNode('Current: ');
+        let hrefValue = '';
 
         if (firstAnchorChild && firstAnchorChild.href.includes('cloudinary')) {
-            hrefValue = firstAnchorChild.href
+            hrefValue = firstAnchorChild.href;
         }
 
-        const imgElement = document.createElement('img')
-        imgElement.src = hrefValue
-        imgElement.alt = 'Your current picture'
-        imgElement.style.borderRadius = '20px'
-        imgElement.style.objectFit = 'cover'
-        imgElement.style.objectPosition = 'center'
-        imgElement.style.height = '150px'
-        imgElement.style.width = '150px'
-        imgElement.style.margin = '20px 0'
+        const imgElement = document.createElement('img');
+        imgElement.src = hrefValue;
+        imgElement.alt = 'Your current picture';
+        imgElement.style.borderRadius = '20px';
+        imgElement.style.objectFit = 'cover';
+        imgElement.style.objectPosition = 'center';
+        imgElement.style.height = '150px';
+        imgElement.style.width = '150px';
+        imgElement.style.margin = '20px 0';
 
         for (let i = 0; i < 8; i++) {
-            imageDivParent.removeChild(imageDivParent.firstChild)
+            imageDivParent.removeChild(imageDivParent.firstChild);
         }
-        imageDivParent.insertBefore(imgElement, imageDivParent.firstChild)
-        imageDivParent.insertBefore(textNode, imgElement)
+        imageDivParent.insertBefore(imgElement, imageDivParent.firstChild);
+        imageDivParent.insertBefore(textNode, imgElement);
 
-        imageDivParent.style.marginBottom = '20px'
+        imageDivParent.style.marginBottom = '20px';
     }
-})
+});
