8d924697b3dcdc2c0f18d02d7818a5094328aea3
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-03-11 15:43:59 +0000

    Edit settings file for linter

--- l2d/settings.py
+++ l2d/settings.py
@@ -140,18 +140,10 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},  # noqa 
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},  # noqa 
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},  # noqa 
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},  # noqa 
 ]
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
