f3f009b374370d87c5629c48326c5c57e0cc616f
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-03-13 19:50:11 +0000

    Adjust settings to allow SQLite to be used for testing

--- l2d/settings.py
+++ l2d/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import cloudinary
 import os
+import sys
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -135,6 +136,9 @@ WSGI_APPLICATION = 'l2d.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
