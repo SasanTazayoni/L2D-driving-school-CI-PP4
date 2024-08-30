12ebb2010472c37e1309cc8e4fb03a8186c82324
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-08-30 16:39:37 +0000

    Update admin panel for Users

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.contrib.auth.models import User
 from .models import UserProfile
 from django_summernote.admin import SummernoteModelAdmin
+
+admin.site.unregister(User)
+
+@admin.register(User)
+class CustomUserAdmin(DefaultUserAdmin):
+    """
+    Custom admin panel for User model to adjust list display.
+    """
+    list_display = ('username', 'email', 'get_name', 'is_staff')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+
+    def get_name(self, obj):
+        return obj.first_name
+    get_name.short_description = 'Name'
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related('profile')
 
 
 @admin.register(UserProfile)
@@ -21,4 +41,4 @@ class UserProfileAdmin(SummernoteModelAdmin):
         updated_count = queryset.update(approved=True)
         self.message_user(
             request, f'{updated_count} profiles have been approved.'
-            )
+        )
